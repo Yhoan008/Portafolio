98cce6a9c03d9b348116881b4402d8dd0cdca02c
Yhoan008 <yhoanmoreno8@gmail.com>
2023-02-27 12:24:57 -0500

    Se baja idiom button

--- src/winter/Navbutton.jsx
+++ src/winter/Navbutton.jsx
@@ -130,7 +130,7 @@ const IdiomButton = styled.button`
   position: absolute;
   right: 0px;
   padding: 0;
-  top: 70px;
+  top: 95px;
   border-radius: 20px;
   background-color: #d19c8a;
   cursor: pointer;
